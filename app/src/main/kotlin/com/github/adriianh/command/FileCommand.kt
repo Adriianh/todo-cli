36871a559c3e10c99bf4ce506c1125e9da4c6099
Adriianh <adrianfunez979@gmail.com>
2024-12-10 01:03:38 -0600

    feat(file-command): change file input from an option to an argument

--- app/src/main/kotlin/com/github/adriianh/command/FileCommand.kt
+++ app/src/main/kotlin/com/github/adriianh/command/FileCommand.kt
@@ -3,13 +3,12 @@ package com.github.adriianh.command
 import com.github.adriianh.common.TaskManager
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.Context
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.arguments.argument
 
 class FileCommand(private val manager: TaskManager) : CliktCommand() {
     override fun help(context: Context) = "Configurate your tasks file."
 
-    private val path by option(help = "File path to save your tasks").required()
+    private val path by argument(help = "File path to save your tasks")
 
     override fun run() {
         manager.setStoragePath(path)
